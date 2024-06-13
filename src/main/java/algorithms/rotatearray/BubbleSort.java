fd1ca4fc6fc95eabfc3813b4fdd5f6f2cc121fd4
flavian-sergiu-ioan <flavian.ioan@gmail.com>
2024-06-13 08:24:00 +0300

    bubble sort

---
+++ src/main/java/algorithms/rotatearray/BubbleSort.java
@@ -0,0 +1,30 @@
+package algorithms.rotatearray;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5,6,7};
+        int k = 3;
+        array = rotate(array, k);
+        print(array);
+    }
+
+    public static int[] rotate(int[] nums, int k) {
+        if (nums == null || nums.length==0 || k < 0) {
+            throw new IllegalArgumentException("Illegal argument!");
+        }
+         for (int i = 0; i< k; i++) {
+             for (int j = nums.length -1; j>0; j--) {
+                 int temp = nums[j];
+                 nums[j] = nums[j-1];
+                 nums[j-1] = temp;
+             }
+         }
+         return nums;
+    }
+
+    public static void print(int[] nums) {
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
